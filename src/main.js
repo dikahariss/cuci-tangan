77e334666a13416ef83fa79af0f169bccc557107
Dika Haris Abdurahman <dika@jsc.co.id>
2020-05-08 17:30:41 +0700

    blank page and add rollup config working

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import App from './App.svelte';
+import 'blueprint-css/dist/blueprint.min.css';
 
 const app = new App({
   target: document.body,
   props: {
-    name: 'world',
+    name: 'Mamang',
   },
 });
 
